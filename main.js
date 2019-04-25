2d2160e3a5a42c25702253c8e4c55a55398ff651
Tuska <nattim2012@gmail.com>
2019-04-25 12:50:33 +0200

    Это наш первый коммит

---
+++ main.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// console.log(4/0);
+// let something;
+// console.log(something);
+
+// let persone = {
+// 	name: 'John',
+// 	age: 25,
+// 	isMarried: false
+// };
+// console.log(persone['name']);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[2]);
+
+// let answer = confirm('Are you here?');
+// console.log(answer);
+
+// let answer = +prompt('Are you here?', 'man');
+// console.log(typeof(answer));
+
+let incr = 10,
+	decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+
+
+// function fun1() {
+// 	var chbox;
+// chbox=document.getElementById('one');
+
+// if (chbox.cheked) {
+// 	alert('Выбран');
+// } else {
+// 	alert('Не выбран');
+// }
+// }
+
+// function fun1() {
+// 	var radi=document.getElementsByName('r1');
+// 	for (var i = 0 ; i < radi.length; i++) {
+// 		if (radi[i].checked) {
+// 			alert('Выбран' +i+ 'элемент');
+// 		}
+// 	}
+// }
+// var fruits = ["Яблоко", "Груша", "Слива"];
+// fruits.push("Груша");
+
+// alert( fruits ); 
+
+
+
+
+
+
+
+
+
+
+// var name = prompt('What is name ecma?', '');
+
+// if (name == 'ECMAScript') {
+//   alert( 'Да вы знаток!' );
+// } else {
+//   alert( 'А вот и неправильно!' ); 
+// }
+
+
+// var value = prompt('Введите число', 0);
+
+// if (value > 0) {
+//   alert( 1 );
+// } else if (value < 0) {
+//   alert( -1 );
+// } else {
+//   alert( 0 );
+// }
+
+// if (age < 3) {
+//   message = 'Здравствуй, малыш!';
+// } else if (age < 18) {
+//   message = 'Привет!';
+// } else if (age < 100) {
+//   message = 'Здравствуйте!';
+// } else {
+//   message = 'Какой необычный возраст!';
+// }
+
